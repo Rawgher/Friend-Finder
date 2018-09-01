7ff300d035845643549e97f946a73ae1b2d245e1
Roger <rdpender3@gmail.com>
2018-08-31 20:50:27 -0700

    started working on apiRoutes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,10 @@
+let path = require("path");
+
+let friends = require('../data/friends.js');
+
+module.exports = function(app) {
+
+    app.get('/api/friends', function(req, res) {
+        res.json(friends);
+    })
+}
